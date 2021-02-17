374dbad92e8041546389b66010c8cbc30980b88c
Aditya Jain <jainaditya400@gmail.com>
2021-02-17 10:02:40 +0530

    added 2 assets in same bundle

--- Assets/Editor/AssetsInfo.cs
+++ Assets/Editor/AssetsInfo.cs
@@ -18,35 +18,45 @@ public class AssetsInfo : EditorWindow
             Component[] comps = obj.GetComponents<Component>();
             foreach(Component comp in comps)
             {
+                int id = comp.GetInstanceID();
                 compList.Add(comp);
+                string guid;
+                long lid;
+                bool find=AssetDatabase.TryGetGUIDAndLocalFileIdentifier(comp, out guid, out lid);
+                Debug.Log(comp.name +" "+comp.GetType()+ " " +" guid: "+guid+">> lid : "+lid+"]]find: "+find);
             }
         }
     
         string[] str = GetAssets();
-        Debug.Log(str.Length + " " + compList.Count);
+        //Debug.Log(str.Length + " " + compList.Count);
+        string assetKey=string.Empty;
+        string assetValue = string.Empty;
+        int count = 0;
         foreach (string s in str)
         {
             if (s.Contains("Assets/"))
             {
                 GUILayout.BeginHorizontal();
-                GUILayout.Label(s.Substring(s.LastIndexOf('/')+1,s.Length- s.LastIndexOf('/')-1));
-                
-    
-                    //foreach (Component com in compList)
-                    //{
-                    //    int id = com.GetInstanceID();
-                    //    string guid;
-                    //    long lid;
-                    //    AssetDatabase.TryGetGUIDAndLocalFileIdentifier(id, out guid, out lid);
-                    //    string guidd = AssetDatabase.AssetPathToGUID(s);
-                    //    Debug.Log("||" + guid);
-                    //    Debug.Log("|>>>|" + guidd);
-                    //    if (guid == guidd)
-                    //    {
-                    //        Debug.Log(s + " --- " + com.name);
-                    //    }
-                    //}
-                
+                //GUILayout.Label();
+                assetKey = "Element " + count.ToString();
+                count++;
+                assetValue = s.Substring(s.LastIndexOf('/') + 1, s.Length - s.LastIndexOf('/') - 1);
+                EditorGUILayout.TextField(assetKey, assetValue);
+              //  foreach (Component com in compList)
+              //  {
+                 //   int id = com.GetInstanceID();
+                 //   string guid;
+                 //   long lid;
+                 //   AssetDatabase.TryGetGUIDAndLocalFileIdentifier(id, out guid, out lid);
+                 //   string guidd = AssetDatabase.AssetPathToGUID(s);
+                 //   Debug.Log("||" + guid);
+                //    Debug.Log("|>>>|" + guidd);
+               //     if (guid == guidd)
+                 //   {
+                //        Debug.Log(s + " --- " + com.name);
+                //    }
+               // }
+
                 GUILayout.EndHorizontal();
             }
         }
